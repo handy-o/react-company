2226afd0574e84b68266fe5f2cff51dc3a98f45f
handy-o <dyhan9420@gmail.com>
2022-05-29 11:52:24 +0900

    popup 호출

--- src/component/sub/Youtube.js
+++ src/component/sub/Youtube.js
@@ -1,6 +1,7 @@
 import Layout from "../common/Layout"
 import { useEffect, useState } from 'react'
 import axios from "axios";
+import Popup from "../common/Popup";
 
 function Youtube() {
     const [vids, setVids] = useState([]);
@@ -8,7 +9,6 @@ function Youtube() {
     const playlist = 'PLcFzhbItLOvYk5e0s4C2eFiO1iiC7hw-Y';
     const num = 5;
     const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
-
     useEffect(() => {
         axios.get(url).then((json) => {
             console.log(json.data.items);
@@ -16,34 +16,42 @@ function Youtube() {
         })
     }, [])
 
+    // 팝업
+    const [open, setOpen] = useState(false);
+
     return (
-        <Layout name={'Youtube'}>
-            {
-                vids.map((vid, idx) => {
-                    // 글자수 자르기
-                    const tit = vid.snippet.title;
-                    const desc = vid.snippet.description;
+        <>
+            <Layout name={'Youtube'}>
+                {
+                    vids.map((vid, idx) => {
+                        // 글자수 자르기
+                        const tit = vid.snippet.title;
+                        const desc = vid.snippet.description;
 
-                    // 구분자로 자르기
-                    const date = vid.snippet.publishedAt;
+                        // 구분자로 자르기
+                        const date = vid.snippet.publishedAt;
 
-                    return (
-                        <article key={idx}>
-                            <h2>{tit.length > 50 ? tit.substr(0, 30) + '...' : tit}</h2>
-                            <div className="txt">
-                                <p>{desc.length > 100 ? desc.substr(0, 200) + '...' : desc}</p>
-                                <span>{date.split('T')[0]}</span>
-                                {/* T를 기점으로 배열로 자름 */}
-                            </div>
-                            <div className="pic">
-                                <img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
-                            </div>
+                        return (
+                            <article key={idx}>
+                                <h2>{tit.length > 20 ? tit.substr(0, 20) + '...' : tit}</h2>
+                                <div className="txt">
+                                    <p>{desc.length > 100 ? desc.substr(0, 200) + '...' : desc}</p>
+                                    <span>{date.split('T')[0]}</span>
+                                    {/* T를 기점으로 배열로 자름 */}
+                                </div>
+                                <div className="pic" onClick={() => setOpen(true)}>
+                                    <img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+                                </div>
 
-                        </article>
-                    )
-                })
-            }
-        </Layout>
+                            </article>
+                        )
+                    })
+                }
+            </Layout>
+
+            {open ? <Popup setOpen={setOpen} /> : null}
+
+        </>
     )
 }
 
