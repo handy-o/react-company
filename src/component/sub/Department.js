7ff632b26acaca3641ea41773374ff8a57c2cf0f
handy-o <dyhan9420@gmail.com>
2022-05-29 09:54:12 +0900

    axios와 json data - member 화면구성하기

--- src/component/sub/Department.js
+++ src/component/sub/Department.js
@@ -1,9 +1,36 @@
 import Layout from "../common/Layout"
+import axios from 'axios';
+import { useState, useEffect } from 'react'; // 외부API
+const path = process.env.PUBLIC_URL;
 
 function Department() {
+    const [members, setMembers] = useState([]);
+
+
+
+    useEffect(() => {
+        axios.get(`${path}/DB/member.json`).then(json => {
+            //console.log(json.data.members)
+            setMembers(json.data.members)
+        })
+    }, [])
+
     return (
         <Layout name={'Department'}>
-            <h1>Department</h1>
+            {/* <h1>Department</h1> */}
+            {members.map((member, idx) => {
+                return (
+                    <article key={idx}>
+                        <div className="inner">
+                            <div className="pic">
+                                <img src={`${path}/img/${member.pic}`} alt={member.name} />
+                            </div>
+                            <h2>{member.name}</h2>
+                            <p>{member.position}</p>
+                        </div>
+                    </article>
+                )
+            })}
         </Layout>
     )
 }
