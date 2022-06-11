9292f56e20637092fdaf0f0a8ecc89f577e29f43
handy-o <dyhan9420@gmail.com>
2022-06-11 11:43:39 +0900

    커뮤니티 - 글 삭제 로직 구현

--- src/component/sub/Community.js
+++ src/component/sub/Community.js
@@ -4,7 +4,17 @@ import { useRef, useState, useEffect } from 'react'
 function Community() {
     const input = useRef(null);
     const textarea = useRef(null);
-    const [Posts, setPosts] = useState([]);
+
+
+    //2)
+    const dummyPosts = [
+        { title: 'Hello5', content: 'Hero comes description in detail' },
+        { title: 'Hello4', content: 'Hero comes description in detail' },
+        { title: 'Hello3', content: 'Hero comes description in detail' },
+        { title: 'Hello2', content: 'Hero comes description in detail' },
+        { title: 'Hello1', content: 'Hero comes description in detail' },
+    ]
+    const [Posts, setPosts] = useState(dummyPosts);
 
     const resetPost = () => {
         // 빈값으로
@@ -26,6 +36,12 @@ function Community() {
         ]);
 
         resetPost();
+    }
+
+    //3 ) 입력한 글 삭제 (배열에서 제외)
+    const deletePost = () => {
+        console.log(index);
+        setPosts(Posts.filter((_, idx) => idx !== index))
     }
 
     useEffect(() => {
@@ -52,6 +68,12 @@ function Community() {
                         <article key={idx}>
                             <h2>{post.title}</h2>
                             <p>{post.content}</p>
+
+                            {/* 3) */}
+                            <div className="btnSet">
+                                <button>EDIT</button>
+                                <button onClick={() => deletePost(idx)}>DELETE</button>
+                            </div>
                         </article>
                     )
                 })}
