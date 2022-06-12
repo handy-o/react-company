208c7e4e222a1e507b69d2c56ff50c66fb414b47
handy-o <dyhan9420@gmail.com>
2022-06-12 10:46:41 +0900

    메인페이지에 Post 데이터 출력

--- src/component/main/News.js
+++ src/component/main/News.js
@@ -1,10 +1,34 @@
+import { useState, useEffect } from 'react';
+
 function News() {
     // 미션 - 
     // 메인의 NEWS 컴포넌트에 커뮤니티 페이지에 있는
     // 최근 게시글 4개를 이곳에 출력
+
+    const getLocalData = () => {
+        const data = localStorage.getItem('post');
+        return JSON.parse(data);
+    }
+
+
+    const [Posts] = useState(getLocalData); // 보여주기만 할거라 setPosts는 사용x
+
+    useEffect(() => {
+
+    }, [])
+
     return (
         <section id="news">
-            Contents
+            {Posts.map((post, idx) => {
+                if (idx < 4) {
+                    return (
+                        <article key={idx}>
+                            <h2>{post.title}</h2>
+                            <p>{post.content}</p>
+                        </article>
+                    )
+                }
+            })}
         </section>
     )
 }
