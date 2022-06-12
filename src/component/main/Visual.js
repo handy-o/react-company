030cd972c8f257ab4265af4e46e39f20ffd9e001
handy-o <dyhan9420@gmail.com>
2022-06-12 11:51:09 +0900

    Anime 애니메이션 추가

--- src/component/main/Visual.js
+++ src/component/main/Visual.js
@@ -1,6 +1,54 @@
+import Anime from '../../asset/anim.js';
+import { useRef } from 'react';
+
 function Visual() {
+    const box = useRef(null);
+    const style = {
+        width: 100,
+        height: 100,
+        backgroundColor: 'aqua',
+        position: 'absolute',
+        top: 100,
+        left: 100,
+    }
     return (
-        <figure id="visual">Visual</figure>
+        <figure id="visual">
+            <div
+                ref={box}
+                className="box"
+                style={style}
+                onClick={(e) => {
+                    // (선택자, prop값)
+                    // new Anime(e.target, {
+                    //     prop: 'opacity',
+                    //     //prop: 'left',
+                    //     value: 400,
+                    //     duration: 500
+                    // })
+
+                    // new Anime(window, {
+                    //     prop: 'scroll',
+                    //     value: 2000,
+                    //     duration: 1500
+                    // })
+
+                    new Anime(box.current, {
+                        prop: 'left',
+                        value: 400,
+                        duration: 500,
+                        callback: () => {
+                            new Anime(box.current, {
+                                prop: 'top',
+                                value: 400,
+                                duration: 500,
+                            })
+                        }
+                    })
+                }}
+            >
+
+            </div>
+        </figure>
     )
 }
 
