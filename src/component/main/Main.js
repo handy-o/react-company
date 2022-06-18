a98b455e9cbe9acc629017b12f157175ffbbd6de
handy-o <dyhan9420@gmail.com>
2022-06-18 10:47:10 +0900

    스크롤시 자식요소 위치값 변경(패럴렉스) - Scrolled값 props를 자식요소 객체에 연동

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -13,7 +13,9 @@ function Main() {
     const main = useRef(null);
     const pos = useRef([]);
     const [Index, setIndex] = useState(0);
+    const [Scrolled, setScrolled] = useState(0);
     let secs = null;
+    let base = null;
 
     const getPos = () => {
         pos.current = [];
@@ -27,7 +29,10 @@ function Main() {
     const activation = () => {
         const scroll = window.scrollY;
         const btns = main.current.querySelectorAll('.scroll_navi li');
-        const base = -(window.innerHeight * 0.3);
+        base = -(window.innerHeight * 0.4);
+        //base = 0;
+
+        setScrolled(scroll);
 
         pos.current.map((pos, idx) => {
             if (scroll >= pos + base) {
@@ -64,7 +69,7 @@ function Main() {
             <Header type={'main'} />
             <Visual />
             <News />
-            <Pics />
+            <Pics Scrolled={Scrolled} start={pos.current[2]} base={base} />
             <Vids />
             <Btns setIndex={setIndex} />
         </main>
--- src/component/main/Pics.js
+++ src/component/main/Pics.js
@@ -1,6 +1,12 @@
-function Pics() {
+import { startTransition } from "react"
+
+function Pics({ Scrolled, start, base }) {
+    console.log(Scrolled)
+    const position = Scrolled - start - base;
     return (
-        <section id="pics" className="myScroll">Pics</section>
+        <section id="pics" className="myScroll">
+            <p style={{ left: 100 + position }}>FLICKER</p>
+        </section>
     )
 }
 
