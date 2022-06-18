a16a8c826ed91f531001c1a56ddca52c5dbde69e
handy-o <dyhan9420@gmail.com>
2022-06-18 10:52:04 +0900

    원하는 위치에서 스타일 적용시작(주석됨)

--- src/component/main/Pics.js
+++ src/component/main/Pics.js
@@ -5,6 +5,7 @@ function Pics({ Scrolled, start, base }) {
     const position = Scrolled - start - base;
     return (
         <section id="pics" className="myScroll">
+            {/* <p style={position >= 0 ? { left: 100 + position } : null}>FLICKER</p> */}
             <p style={{ left: 100 + position }}>FLICKER</p>
         </section>
     )
