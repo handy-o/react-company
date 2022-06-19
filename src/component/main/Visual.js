4a67cdbfc7788602d087117117ded0b37bb36fe4
handy-o <dyhan9420@gmail.com>
2022-06-19 09:23:13 +0900

    prev 슬라이드 구현

--- src/component/main/Visual.js
+++ src/component/main/Visual.js
@@ -6,58 +6,59 @@ function Visual() {
     let panel_li = null;
     let len = null;
 
+    const showPrev = () => {
+        panel_li = panel.current.children;
+        len = panel_li.length;
+        const currentEl = panel.current.querySelector('.on');
+        const current_index = Array.from(panel_li).indexOf(currentEl);
 
+        let prev_index = null;
+        current_index !== 0
+            ? (prev_index = current_index - 1)
+            : (prev_index = len - 1);
+
+        showSlide(currentEl, prev_index, -1);
+    };
 
     const showNext = () => {
         panel_li = panel.current.children;
         len = panel_li.length;
         const currentEl = panel.current.querySelector('.on');
-        const current_inedex = Array.from(panel_li).indexOf(currentEl); // .from유사배열을? 순서배열로?
-        console.log(current_index); // 클릭했을 때 순번 idx
-        //현재 순번, 다음순번 
+        const current_index = Array.from(panel_li).indexOf(currentEl);
         let next_index = null;
-        current_index !== len - 1 // 마지막 순번이 아니면 
-            ? next_index = current_index + 1 // 현재에 + 1
-            : next_index = 0;	// 끝이니까 다음은 처음으로 0
+        current_index !== len - 1
+            ? (next_index = current_index + 1)
+            : (next_index = 0);
 
-        showSlide(currentEl, next_index);
-    }
+        showSlide(currentEl, next_index, 1);
+    };
 
-
-    // 만약에 dir값이 플러스면  >>
-    // 만약에 dir값이 마이너스면 << 
     const showSlide = (el, index, direction) => {
         panel_li = panel.current.children;
-
-        // 기존 환성화 패널 왼쪽 밖으로 모션 이동
+        //기존 활성화 패널  왼쪽 밖으로 모션 이동
         new Anime(el, {
             prop: 'left',
             value: -direction * 100 + '%',
             duration: 500,
             callback: () => {
                 el.classList.remove('on');
-                el.style.display = 'none';
-            }
+                el.style.diplay = 'none';
+            },
         });
 
         panel_li[index].style.display = 'flex';
         panel_li[index].style.left = direction * 100 + '%';
 
-
-        // 앞으로 활성화 될 패널 프레임 안쪽으로 모션 이동
+        //앞으로 활성화될 패널 프레임 안쪽으로 모션 이동
         new Anime(panel_li[index], {
             prop: 'left',
             value: '0%',
             duration: 500,
             callback: () => {
-                el.classList.remove('on');
-                el.style.display = 'none';
-            }
+                panel_li[index].classList.add('on');
+            },
         });
-
-        panel_li[index].style.display = 'flex';
-        panel_li[index].style.left = direction * 100 + '%';
-    }
+    };
 
     return (
         <figure id='visual' className='myScroll'>
