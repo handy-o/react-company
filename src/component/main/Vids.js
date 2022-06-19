e9860498600573d07a15ed83e375a9dcc963fd32
handy-o <dyhan9420@gmail.com>
2022-06-19 11:28:40 +0900

    자주쓰는 스와피어 옵션 기능 추가

--- src/component/main/Vids.js
+++ src/component/main/Vids.js
@@ -1,6 +1,30 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
 function Vids() {
     return (
-        <section id="vids" className="myScroll">Vids</section>
+        <section id="vids" className="myScroll">
+            <Swiper
+                navigation={true}
+                pagination={{ clickable: true }}
+                modules={[Navigation, Pagination]}
+                loop={true} // 처음-끝 반복
+                spaceBetween={50} // 슬라이드 간 간격
+                slidesPerView={3} // 한번에 보여지는 슬라이드 개수
+                centeredSlides={true} // 센터모드
+            >
+                <SwiperSlide>1</SwiperSlide>
+                <SwiperSlide>2</SwiperSlide>
+                <SwiperSlide>3</SwiperSlide>
+                <SwiperSlide>4</SwiperSlide>
+                <SwiperSlide>5</SwiperSlide>
+                <SwiperSlide>6</SwiperSlide>
+            </Swiper>
+
+        </section>
     )
 }
 
