becd804b18d7d7152d25dc2a8c8d798fc76e4acd
handy-o <dyhan9420@gmail.com>
2022-06-25 10:31:20 +0900

    search flickr 추가(인수로 메서드명에 따라 갤러리 데이터 반환하도록 함수 수정)

--- src/component/sub/Gallery.js
+++ src/component/sub/Gallery.js
@@ -5,20 +5,24 @@ import { useEffect, useState, useRef } from 'react'
 function Gallery() {
     const key = '418715e184dbd270f5ea19ff1fa3672f';
     const method_interest = 'flickr.interestingness.getList';
+    const method_search = 'flickr.photos.search'
     const num = 50;
     //https://www.flickr.com/services/api/request.rest.html
-    const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${num}&format=json&nojsoncallback=1`;
+    const url_interest = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${num}&format=json&nojsoncallback=1`;
+    const url_search = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${key}&per_page=${num}&tags=ocean&format=json&nojsoncallback=1`;
     const frame = useRef(null);
     const [Items, setItems] = useState([]);
 
-    const getFlickr = async () => {
+    const getFlickr = async (url) => {
         await axios.get(url).then(json => {
             console.log(json.data.photos.photo);
             setItems(json.data.photos.photo)
         })
         frame.current.classList.add('on')
     }
-    useEffect(getFlickr, []); //함수라서 arrow다시 랩핑 안해도 됨
+    useEffect(() => {
+        getFlickr(url_search)
+    }, []); //함수라서 arrow다시 랩핑 안해도 됨
 
     // per.page  몇개
     return (
