9c411335559fd1beca0df21f278f474e217d0078
handy-o <dyhan9420@gmail.com>
2022-06-25 10:21:01 +0900

    flickr갤러리_getFlickr()

--- src/component/sub/Gallery.js
+++ src/component/sub/Gallery.js
@@ -1,27 +1,31 @@
 import axios from 'axios'
 import Layout from "../common/Layout"
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 function Gallery() {
     const key = '418715e184dbd270f5ea19ff1fa3672f';
     const method_interest = 'flickr.interestingness.getList';
     const num = 50;
-
     //https://www.flickr.com/services/api/request.rest.html
-    const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${num}&format=json&nojsoncallback=1`
-
+    const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${num}&format=json&nojsoncallback=1`;
+    const frame = useRef(null);
     const [Items, setItems] = useState([]);
-    useEffect(() => {
-        axios.get(url).then(json => {
+
+    const getFlickr = async () => {
+        await axios.get(url).then(json => {
             console.log(json.data.photos.photo);
             setItems(json.data.photos.photo)
         })
-    }, []);
+        frame.current.classList.add('on')
+    }
+    useEffect(getFlickr, []); //함수라서 arrow다시 랩핑 안해도 됨
 
     // per.page  몇개
     return (
         <Layout name={'Gallery'}>
-            <ul>
+            <button>Interest Gallery</button>
+            <button>Search Gallery</button>
+            <ul ref={frame}>
                 {Items.map((item) => {
                     return (
                         <li key={item.id}>
