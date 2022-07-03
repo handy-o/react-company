f2017cd71f612683e63162d7dfc25a2154646663
handy-o <dyhan9420@gmail.com>
2022-07-03 09:12:45 +0900

    Vids영역에서만 마우스포인터 실행

--- src/component/main/Vids.js
+++ src/component/main/Vids.js
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination } from 'swiper'
+import { Navigation, Pagination, Autoplay } from 'swiper'
 import { useRef, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import 'swiper/css'
@@ -9,24 +9,39 @@ import 'swiper/css/pagination'
 function Vids() {
     const { youtube } = useSelector((store) => store.youtubeReducer)
     const cursor = useRef(null);
+    const frame = useRef(null);
+    let isCursor = false;
     const mouseMove = e => {
+        if (!isCursor) return;
         cursor.current.style.left = e.clientX + 'px';
         cursor.current.style.top = e.clientY + 'px';
     }
     useEffect(() => {
         window.addEventListener('mousemove', mouseMove);
+        frame.current.addEventListener('mouseenter', () => {
+            isCursor = true;
+            cursor.current.style.display = 'block';
+        })
+        frame.current.addEventListener('mouseleave', () => {
+            isCursor = false;
+            cursor.current.style.display = 'none';
+        })
         return () => window.removeEventListener('mousemove', mouseMove);
     }, [])
     return (
-        <section id="vids" className="myScroll">
+        <section id="vids" className="myScroll" ref={frame}>
             <Swiper
                 navigation={true}
                 pagination={{ clickable: true }}
-                modules={[Navigation, Pagination]}
+                modules={[Navigation, Pagination, Autoplay]}
                 loop={true} // 처음-끝 반복
                 spaceBetween={50} // 슬라이드 간 간격
                 slidesPerView={3} // 한번에 보여지는 슬라이드 개수
                 centeredSlides={true} // 센터모드
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false
+                }}
                 breakpoints={{
                     500: {
                         slidesPerView: 1,
@@ -49,11 +64,13 @@ function Vids() {
                                     <img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
                                 </div>
                             </div>
-
                         </SwiperSlide>
                     )
                 })}
             </Swiper>
+            {/* {youtube.map((vid, idx) => {
+                <img key={idx} src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+            })} */}
             <div className="cursor" ref={cursor}></div>
         </section>
     )
